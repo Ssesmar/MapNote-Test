2804ffe0834eeb8542ce1834544b5aa0d97991d6
BadBoyBarny <119746693+Ssesmar@users.noreply.github.com>
2023-12-22 17:54:58 +0100

    Add files via upload

--- main.lua
+++ main.lua
@@ -139,38 +139,8 @@ function MapNotesMiniButton:OnInitialize()
     print(COLORED_ADDON_NAME .. "|cffffff00  " .. L["-> MiniMapButton <-"], "|cffff0000" .. L["is deactivated"])
   end
 
-  local miniButton = {
-  text = COLORED_ADDON_NAME,
-  type = "data source",
-  icon = iconLink .. "MN_Logo",
-  OnTooltipShow = function(tooltip)
-    if not tooltip or not tooltip.AddLine then return end
-    tooltip:AddLine(COLORED_ADDON_NAME)
-    tooltip:AddLine(" ")
-    tooltip:AddLine(L["Left-click => Open/Close"] .. " " .. COLORED_ADDON_NAME,1,1,1)
-    tooltip:AddLine(L["Shift + Right-click => hide"] .. "|cffff0000  " .. L["-> MiniMapButton <-"],1,1,1)
-    tooltip:AddLine("|cffffff00" .. L["(also opens MapNotes if hiding was unwanted)"],1,1,1,1)       
-    end,
-  OnClick = function(self, button)
-    if IsShiftKeyDown() and button == "RightButton" then
-      MNMMBIcon:Hide("MNMiniMapButton")
-      db.show.HideMMB = true  
-      LibStub("AceConfigDialog-3.0"):Open("MNMiniMapButton")
-      print(COLORED_ADDON_NAME .. "|cffffff00  " .. L["-> MiniMapButton <-"], "|cffff0000" .. L["is deactivated"]) 
-      print(COLORED_ADDON_NAME.."|cffffff00  ".. L["MapNotes menu window"], "|cff00ff00" .. L["is activated"])  
-      
-    end
-    if button == "LeftButton" then
-      if not LibStub("AceConfigDialog-3.0"):Close("MNMiniMapButton") then
-        LibStub("AceConfigDialog-3.0"):Open("MNMiniMapButton") 
-        print(COLORED_ADDON_NAME.."|cffffff00  ".. L["MapNotes menu window"], "|cff00ff00" .. L["is activated"])
-      end 
-    end
-          end 
-                  } 
-
   self.db = LibStub("AceDB-3.0"):New("MNMiniMapButtonDB", { profile = { minimap = { hide = false, }, }, }) 
-  MNMMBIcon:Register("MNMiniMapButton", miniButton, self.db.profile.minimap)
+  MNMMBIcon:Register("MNMiniMapButton", ns.miniButton, self.db.profile.minimap)
   
 end
 
